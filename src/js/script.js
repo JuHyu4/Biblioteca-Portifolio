916eb0e2a38e2a6677f145b1845284a221e8d9b0
Julyana Torres <julyanasstrr@gmail.com>
2025-11-26 22:27:30 -0300

    adicionando slider em habilidades

--- src/js/script.js
+++ src/js/script.js
@@ -14,3 +14,26 @@ menuButton.addEventListener("click", () => {
     menuButton.classList.toggle("active");
     menuNav.classList.toggle("active");
 });
+
+const slides = document.querySelectorAll(".habilidade");
+const prevBtn = document.getElementById("prevSlide");
+const nextBtn = document.getElementById("nextSlide");
+
+let index = 0;
+
+function showSlide(i) {
+    slides.forEach(slide => slide.style.display = "none");
+    slides[i].style.display = "block";
+}
+
+nextBtn.addEventListener("click", () => {
+    index = (index + 1) % slides.length;
+    showSlide(index);
+});
+
+prevBtn.addEventListener("click", () => {
+    index = (index - 1 + slides.length) % slides.length;
+    showSlide(index);
+});
+
+showSlide(index);
